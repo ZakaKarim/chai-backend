d00078859eb68344a305854f75c3ab9e77c506d4
Zaka_Karim <link3zakakarim@gmail.com>
2024-11-23 04:02:23 +0500

    Setup Done

--- src/app.js
+++ src/app.js
@@ -15,4 +15,12 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//import routes
+import userRouter from "./routes/user.routes.js";
+
+// router Declarations
+
+app.use("/api/v1/users", userRouter);
+
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 // import dotenv from "dotenv";
 import "dotenv/config";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 //well is ma error aya tha but i slove it thanks to Allah well agar tum second wala
 // way se dotenv ko config kar raha ho tu is ko comment kar do your code will work
 //but agar phle wala way hai to is ko use karnai ho gay otherwise error aya gay
 // dotenv.config({
-//   path: "./.env",
+//   path: "./env",
 // });
 
 connectDB()
